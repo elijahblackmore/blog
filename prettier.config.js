6e04731f5c509aae24bca896b4e80bca1e4cfeea
Elijah Blackmore <elijah.blackmore@simplygreat.education>
2024-03-24 12:33:51 +0000

    feat(deps): add tailwindcss, prettier and configuration files

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// prettier.config.js
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/**/*.{js}", "./src/app/**/*.{js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
