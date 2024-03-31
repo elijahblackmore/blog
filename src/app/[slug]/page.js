f187774bd14089a1a251b440b80dddb52979fd60
Elijah Blackmore <elijah.blackmore@simplygreat.education>
2024-03-31 14:24:28 +0100

    feat: update post slug page

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 import { compileMDX } from "next-mdx-remote/rsc";
-import { formatDistanceStrict } from "date-fns";
+import { format } from "date-fns";
 import { getPosts, getPostBySlug } from "@/lib/posts";
+import "@/styles/markdown.css";
 
 export default async function Post({ params }) {
   const post = getPostBySlug(params.slug);
@@ -11,22 +16,66 @@ export default async function Post({ params }) {
     options: {
       mdxOptions: {
         remarkPlugins: [],
-        rehypePlugins: [rehypePrettyCode],
+        rehypePlugins: [
+          rehypeSlug,
+          [
+            rehypeAutolinkHeadings,
+            {
+              behavior: "wrap",
+              properties: {
+                ariaLabel: "Link to subheading",
+                className: ["subheading-anchor"],
+              },
+            },
+          ],
+          [
+            rehypePrettyCode,
+            {
+              theme: {
+                dark: "catppuccin-mocha",
+                light: "catppuccin-latte",
+              },
+            },
+          ],
+        ],
       },
     },
   });
 
   return (
     <main>
-      <time className="font-mono text-sm" dateTime={post.date}>
-        {formatDistanceStrict(post.date, new Date(), {
-          addSuffix: true,
-        })}
+      <Link href="/" className="flex items-center gap-x-1.5">
+        <ChevronLeft
+          size={14}
+          strokeWidth={3}
+          className="text-light-icon dark:text-dark-icon"
+        />
+        <span className="text-sm font-medium text-light-body dark:text-dark-body">
+          Back to posts
+        </span>
+      </Link>
+      <h1 className="my-4 text-2xl font-extrabold text-light-heading dark:text-dark-heading">
+        {post.title}
+      </h1>
+      <time
+        className="text-sm text-light-body dark:text-dark-body"
+        dateTime={post.date}
+      >
+        Posted on {format(post.date, `MMMM dd, yyyy`)}
       </time>
-      <h1 className="mb-8 mt-1 text-2xl">{post.title}</h1>
-      {content}
+      <section className="prose prose-neutral pt-12 dark:prose-invert">
+        {content}
+      </section>
     </main>
   );
+}
+
+export async function generateMetadata({ params }) {
+  const post = getPostBySlug(params.slug);
+
+  return {
+    title: `${post.title} | Elijah Blackmore`,
+  };
 }
 
 export async function generateStaticParams() {
