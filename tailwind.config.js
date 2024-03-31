c2aebfe7dd3e2a0b5a576a054dca53b4bae8c32f
Elijah Blackmore <elijah.blackmore@simplygreat.education>
2024-03-31 14:00:37 +0100

    feat: update tailwind config with theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/components/**/*.js", "./src/app/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      textDecorationColor: {
+        light: {
+          underline: "#CAC4D0",
+        },
+        dark: {
+          underline: "#49454F",
+        },
+      },
+      colors: {
+        light: {
+          surface: "#FEF7FF",
+          heading: "#1D1B20",
+          body: "49454F",
+          icon: "79747E",
+        },
+        dark: {
+          surface: "#141218",
+          heading: "#E6E0E9",
+          body: "#CAC4D0",
+          icon: "#938F99",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
