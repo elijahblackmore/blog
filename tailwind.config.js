3133028fdc859a122dd2f92c3bb8cf80572f051e
Elijah Blackmore <elijah.blackmore@simplygreat.education>
2024-04-12 14:35:20 +0100

    feat: add space grotesk font and update prose classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
 				theme: {
 					css: {
 						a: {
-							textDecoration: "wavy underline", // Change underline style here
+							textDecoration: "wavy underline",
 							textUnderlineOffset: "4px",
 						},
 						"--tw-prose-body": theme("colors.light[body]"),
 						"--tw-prose-headings": theme("colors.light[heading]"),
 						"--tw-prose-lead": theme("colors.pink[700]"),
 						"--tw-prose-links": theme("colors.light[heading]"),
-						"--tw-prose-bold": theme("colors.pink[900]"),
+						"--tw-prose-bold": theme("colors.light[heading]"),
 						"--tw-prose-counters": theme("colors.dark[underline]"),
 						"--tw-prose-bullets": theme("colors.pink[400]"),
 						"--tw-prose-hr": theme("colors.pink[300]"),
@@ -27,10 +27,10 @@ module.exports = {
 						"--tw-prose-th-borders": theme("colors.pink[300]"),
 						"--tw-prose-td-borders": theme("colors.pink[200]"),
 						"--tw-prose-invert-body": theme("colors.dark[body]"),
-						"--tw-prose-invert-headings": theme("colors."),
+						"--tw-prose-invert-headings": theme("colors.dark[heading]"),
 						"--tw-prose-invert-lead": theme("colors.pink[300]"),
 						"--tw-prose-invert-links": theme("colors.dark[heading]"),
-						"--tw-prose-invert-bold": theme("colors.white"),
+						"--tw-prose-invert-bold": theme("colors.dark[heading]"),
 						"--tw-prose-invert-counters": theme("colors.dark[heading]"),
 						"--tw-prose-invert-bullets": theme("colors.pink[600]"),
 						"--tw-prose-invert-hr": theme("colors.pink[700]"),
@@ -38,7 +38,7 @@ module.exports = {
 						"--tw-prose-invert-quote-borders": theme("colors.dark[underline]"),
 						"--tw-prose-invert-captions": theme("colors.pink[400]"),
 						"--tw-prose-invert-code": theme("colors.dark[heading]"),
-						"--tw-prose-invert-pre-code": theme("colors.pink[300]"),
+						"--tw-prose-invert-pre-code": theme("colors.dark[heading]"),
 						"--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
 						"--tw-prose-invert-th-borders": theme("colors.pink[600]"),
 						"--tw-prose-invert-td-borders": theme("colors.pink[700]"),
@@ -46,28 +46,28 @@ module.exports = {
 				},
 			}),
 			fontFamily: {
-				sans: ["var(--font-outfit)"],
+				sans: ["var(--font-space-grotesk)"],
 				mono: ["var(--font-jetbrains-mono)"],
-			},
-
-			textDecorationColor: {
-				light: {
-					underline: "#CAC4D0",
-				},
-				dark: {
-					underline: "#49454F",
-				},
 			},
 			colors: {
 				light: {
+					underline: "#CAC4D0",
 					surface: "#FEF7FF",
+					elevation: {
+						20: "#ECE6F0",
+					},
 					heading: "#1D1B20",
-					body: "49454F",
-					icon: "79747E",
+					body: "#49454F",
+					icon: "#79747E",
 				},
 				dark: {
 					underline: "#49454F",
 					surface: "#141218",
+					elevation: {
+						10: "#1D1B20",
+						20: "#211F26",
+						30: "#2B2930",
+					},
 					heading: "#E6E0E9",
 					body: "#CAC4D0",
 					icon: "#938F99",
@@ -76,4 +76,5 @@ module.exports = {
 		},
 	},
 	plugins: [require("@tailwindcss/typography")],
+	darkMode: "class",
 };
