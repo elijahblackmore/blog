6ecfeb094213fa5811032685bdccae3351afe650
Elijah Blackmore <elijah.blackmore@simplygreat.education>
2024-04-14 15:49:07 +0100

    refactor: replace wavy underline due to different browser styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import { getPosts, groupPostsByYear } from "@/lib/posts";
-import { Scale } from "lucide-react";
-import Link from "next/link";
 import Article from "@/components/Article";
 import ThemeSwitch from "@/components/ThemeSwitch";
+import { Scale } from "lucide-react";
+import Link from "next/link";
 
 export default function Page() {
   const posts = getPosts();
@@ -11,41 +11,36 @@ export default function Page() {
     <>
       <section className="flex items-center justify-between">
         <div className="flex items-center gap-x-2.5">
-          <Scale size={18} className="text-light-icon dark:text-dark-icon" />
+          <Scale
+            size={18}
+            className="text-light-icon-variant dark:text-dark-icon-variant"
+          />
           <Link
             href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
             target="_blank"
             rel="noopener noreferrer"
-            className="font-mono text-sm font-medium text-light-body underline decoration-light-icon decoration-wavy underline-offset-4 dark:text-dark-heading dark:decoration-dark-icon"
+            className="font-mono text-sm font-medium text-light-heading underline decoration-light-underline underline-offset-[6px] dark:text-dark-heading dark:decoration-dark-underline"
           >
             CC BY-NC-SA 4.0
           </Link>
         </div>
         <ThemeSwitch />
-        {/*
-        <Link
-          href="https://github.com/elijahblackmore/blog"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-mono text-sm font-medium text-light-body underline decoration-light-underline decoration-wavy underline-offset-4 dark:text-dark-heading dark:decoration-dark-icon"
-        >
-          Source code
-        </Link>
-        */}
       </section>
       <div className="mt-8 space-y-6">
         {Object.entries(groupPostsByYear(posts))
           .sort(([b], [a]) => Number(a) - Number(b))
           .map(([year, postsOfYear]) => (
-            <div className="space-y-4 pt-8 md:space-y-4">
-              <h2 className="font-medium text-light-icon dark:text-dark-icon">
+            <div className="space-y-6 pt-8" key={year}>
+              <h2 className="text-xl font-bold text-light-heading-variant dark:text-dark-heading-variant">
                 {year}
               </h2>
-              <div className="space-y-6 md:space-y-4">
+              <ul className="space-y-6 md:space-y-6">
                 {postsOfYear.map((post) => (
-                  <Article post={post} />
+                  <li key={post.slug}>
+                    <Article post={post} />
+                  </li>
                 ))}
-              </div>
+              </ul>
             </div>
           ))}
       </div>
